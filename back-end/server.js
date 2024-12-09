abdc0f4182403bbfcc6a27c1aad975219db43a00
Purushotham161819 <mpurushotham98@gmail.com>
2024-12-09 13:08:25 -0600

    Commiting the code the API Documentation

--- back-end/server.js
+++ back-end/server.js
@@ -1,5 +1,10 @@
 const { server, port, host } = require('./dependencies');
 const routes = require('./routes/routes');  // Import the routes
+const { swaggerUi, swaggerSpec } = require('./swaggerConfig');
+
+
+server.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 
 // Use the routes for the /api path
 server.use('/api', routes);
